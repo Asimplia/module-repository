387c4ced3f0a2fd4c1f7427394f784b4d5005bbb
Michael Zabka <zabka.michael@seznam.cz>
2014-07-08 21:38:05 +0200

    Add node-sqlserver to repository & copy to node_modules after npm install

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,37 +1,59 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    ts: {
-        // A specific target
-        build: {
-            // The source TypeScript files, http://gruntjs.com/configuring-tasks#files
-            src: ["**/*.ts", "!**/*.d.ts", "!node_modules/**/*.ts"],
-            // If specified, the generate JavaScript files are placed here. Only works if out is not specified
-            outDir: '',
-            // If specified, watches this directory for changes, and re-runs the current target
-            watch: '.',                     
-            // Use to override the default options, http://gruntjs.com/configuring-tasks#options
-            options: {     
-                // 'es3' (default) | 'es5'
-                target: 'es5',
-                // 'amd' (default) | 'commonjs'    
-                module: 'commonjs',
-                // true (default) | false
-                sourceMap: false,
-                // true | false (default)
-                declaration: false,
-                // true (default) | false
-                removeComments: false
+    var tsFiles = ["**/*.ts", "!**/*.d.ts", "!node_modules/**/*.ts"];
+
+    // Project configuration.
+    grunt.initConfig({
+        ts: {
+            // A specific target
+            build: {
+                src: tsFiles,
+                outDir: '',               
+                options: {     
+                    // 'es3' (default) | 'es5'
+                    target: 'es5',
+                    // 'amd' (default) | 'commonjs'    
+                    module: 'commonjs',
+                    // true (default) | false
+                    sourceMap: false,
+                    // true | false (default)
+                    declaration: false,
+                    // true (default) | false
+                    removeComments: false
+                },
+            }
+        },
+        copy: {
+            main: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'node_modules/node-sqlserver/',
+                        src: ['**'], 
+                        dest: 'build_modules/node-sqlserver/'
+                    }
+                ]
+            }
+        },
+        watch: {
+            ts: {
+                files: tsFiles.concat('node_modules/node-sqlserver/**'),
+                tasks: ['ts:build', 'copy'],
+                options: {
+                    livereload: true,
+                    debug: false,
+                    debounceDelay: 100
+                }
             },
         }
-    },
-  });
+    });
 
-  grunt.loadNpmTasks('grunt-ts');
+    grunt.loadNpmTasks('grunt-ts');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', [
-    'ts:build'
-  ]);
+    grunt.registerTask('default', [
+        'ts:build', 'copy', 'watch'
+    ]);
 
 };
