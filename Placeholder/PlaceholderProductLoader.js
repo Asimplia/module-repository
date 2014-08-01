53da948f3804a9c6b64e86a745424de42d39b4b2
Michael Zabka <zabka.michael@seznam.cz>
2014-08-01 21:14:42 +0200

    Fix get product name from graph

--- Placeholder/PlaceholderProductLoader.js
+++ Placeholder/PlaceholderProductLoader.js
@@ -7,15 +7,51 @@ var PlaceholderProductLoader = (function () {
             _this.db = db;
         });
     }
-    PlaceholderProductLoader.prototype.getProductName = function (productId, callback) {
-        this.db.query('MATCH (a:Product) WHERE (a.id = "{productId}") RETURN a', {
+    PlaceholderProductLoader.prototype.getName = function (productId, callback) {
+        this.db.query('MATCH (c:Customer {name: "Karel Havlena"}) RETURN c.name;', {
             productId: productId
-        }, function (e, productName) {
+        }, function (e, res) {
             if (e) {
                 callback(e);
                 return;
             }
-            callback(null, productName);
+            callback(null, res.pop()['c.name']);
+        });
+    };
+
+    PlaceholderProductLoader.prototype.getPrice = function (productId, callback) {
+        this.db.query('MATCH (a:Product) WHERE (a.id = "{productId}") RETURN a', {
+            productId: productId
+        }, function (e, price) {
+            if (e) {
+                callback(e);
+                return;
+            }
+            callback(null, price);
+        });
+    };
+
+    PlaceholderProductLoader.prototype.getPackageOption = function (productId, callback) {
+        this.db.query('MATCH (a:Product) WHERE (a.id = "{productId}") RETURN a', {
+            productId: productId
+        }, function (e, productNames) {
+            if (e) {
+                callback(e);
+                return;
+            }
+            callback(null, productNames);
+        });
+    };
+
+    PlaceholderProductLoader.prototype.getStokingTime = function (productId, callback) {
+        this.db.query('MATCH (a:Product) WHERE (a.id = "{productId}") RETURN a', {
+            productId: productId
+        }, function (e, stokingTime) {
+            if (e) {
+                callback(e);
+                return;
+            }
+            callback(null, stokingTime);
         });
     };
     return PlaceholderProductLoader;
--- Placeholder/PlaceholderProductLoader.ts
+++ Placeholder/PlaceholderProductLoader.ts
@@ -13,15 +13,51 @@ class PlaceholderProductLoader {
 		});
 	}
 
-	getProductName(productId: number, callback: (e: Error, productName?: string) => void): void {
-		this.db.query('MATCH (a:Product) WHERE (a.id = "{productId}") RETURN a', {
+	getName(productId: number, callback: (e: Error, productName?: string) => void): void {
+		this.db.query('MATCH (c:Customer {name: "Karel Havlena"}) RETURN c.name;', {
 			productId: productId
-		}, (e: Error, productName: string) => {
+		}, (e: Error, res) => {
 			if (e) {
 				callback(e);
 				return;
 			}
-			callback(null, productName);
+			callback(null, res.pop()['c.name']);
+		});
+	}
+
+	getPrice(productId: number, callback: (e: Error, price?: number) => void): void {
+		this.db.query('MATCH (a:Product) WHERE (a.id = "{productId}") RETURN a', {
+			productId: productId
+		}, (e: Error, price: number) => {
+			if (e) {
+				callback(e);
+				return;
+			}
+			callback(null, price);
+		});
+	}
+
+	getPackageOption(productId: number, callback: (e: Error, productNames?: string[]) => void): void {
+		this.db.query('MATCH (a:Product) WHERE (a.id = "{productId}") RETURN a', {
+			productId: productId
+		}, (e: Error, productNames: string[]) => {
+			if (e) {
+				callback(e);
+				return;
+			}
+			callback(null, productNames);
+		});
+	}
+
+	getStokingTime(productId: number, callback: (e: Error, stokingTime?: number) => void): void {
+		this.db.query('MATCH (a:Product) WHERE (a.id = "{productId}") RETURN a', {
+			productId: productId
+		}, (e: Error, stokingTime: number) => {
+			if (e) {
+				callback(e);
+				return;
+			}
+			callback(null, stokingTime);
 		});
 	}
 
