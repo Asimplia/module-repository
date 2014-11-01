3fbbf1e3d36b820ed34de17c34027c7505a98675
Michael Zabka <zabka.michael@seznam.cz>
2014-11-01 18:50:14 +0100

    Add Sql executor removeBy

--- src/Util/SqlExecutor.ts
+++ src/Util/SqlExecutor.ts
@@ -3,6 +3,7 @@ import EntityPreparer = require('../Entity/EntityPreparer');
 import List = require('../Entity/List');
 import IEntity = require('../Entity/IEntity');
 import _ = require('underscore');
+import moment = require('moment');
 
 export = SqlExecutor;
 class SqlExecutor {
@@ -44,5 +45,65 @@ class SqlExecutor {
 			}
 			callback(null, list);
 		});
-	}	
+	}
+
+	removeBy(conditions: any, callback: (e: Error) => void) {
+		var where = this.getWhereByConditions(conditions);
+		var sql = 'DELETE FROM ' + this.EntityStatic.TABLE_NAME + ' '
+			+ ' WHERE ' + where.sql;
+		this.connection.query(sql, where.params, (e: Error) => {
+			if (e) {
+				callback(e);
+				return;
+			}
+			callback(null);
+		});
+	}
+
+	private getWhereByConditions(conditions: any): { sql: string; params: any[] } {
+		var params = [];
+		var whereParts = [];
+		var placeholderIndex = 0;
+		_.each(Object.keys(conditions), (key: string) => {
+			var value = conditions[key];
+			var column = EntityPreparer.getTableColumnByKey(this.EntityStatic, key);
+			if (typeof value === 'object') {
+				if (typeof value.$gt !== 'undefined') {
+					placeholderIndex++;
+					whereParts.push(' ' + column + ' > $' + placeholderIndex) + ' ';
+					params.push(this.prepareValue(value.$gt));
+				}
+				if (typeof value.$lt !== 'undefined') {
+					placeholderIndex++;
+					whereParts.push(' ' + column + ' < $' + placeholderIndex) + ' ';
+					params.push(this.prepareValue(value.$lt));
+				}
+				if (typeof value.$gte !== 'undefined') {
+					placeholderIndex++;
+					whereParts.push(' ' + column + ' >= $' + placeholderIndex) + ' ';
+					params.push(this.prepareValue(value.$gte));
+				}
+				if (typeof value.$lte !== 'undefined') {
+					placeholderIndex++;
+					whereParts.push(' ' + column + ' <= $' + placeholderIndex) + ' ';
+					params.push(this.prepareValue(value.$lte));
+				}
+			} else {
+				placeholderIndex++;
+				whereParts.push(' ' + column + ' = $' + placeholderIndex) + ' ';
+				params.push(this.prepareValue(value));
+			}
+		});
+		return {
+			sql: whereParts.join(' AND '),
+			params: params
+		};
+	}
+
+	private prepareValue(value: any) {
+		if (value instanceof Date) {
+			return moment(value).add(value.getTimezoneOffset(), 'minutes').toDate();
+		}
+		return value;
+	}
 }
