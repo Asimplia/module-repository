b85181ec079a5fc09e54236ae4e9a834dc34b639
Michael Zabka <zabka.michael@gmail.com>
2015-01-06 01:09:06 +0100

    Add waiting until typings files are installed + add link module util

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,28 +2,35 @@ module.exports = function (grunt) {
 	var GruntConfiguration = require('asimplia-util').GruntConfiguration;
 
 	var typescriptBuildFiles = ["src/**/*.ts", "tests/**/*.ts", "!node_modules/**/*.ts"];
-
 	// Project configuration.
-	var config = GruntConfiguration([], [], [], [
-		'typescript:build', 'jasmine_node:unit'
-	], typescriptBuildFiles, typescriptBuildFiles, [
+	var config = GruntConfiguration([], [], [], typescriptBuildFiles, typescriptBuildFiles, [
 		'typings/tsd.d.ts'
-	]);
+	], __dirname);
 	grunt.initConfig(config);
 
 	GruntConfiguration.loadParentNpmTasks(grunt, 'grunt-typescript');
 	GruntConfiguration.loadParentNpmTasks(grunt, 'grunt-jasmine-node');
 	GruntConfiguration.loadParentNpmTasks(grunt, 'grunt-contrib-watch');
 	GruntConfiguration.loadParentNpmTasks(grunt, 'grunt-tsd');
+	GruntConfiguration.loadParentNpmTasks(grunt, 'grunt-shell');
+	GruntConfiguration.loadParentNpmTasks(grunt, 'grunt-wait');
 
 	grunt.registerTask('default', [
-		'tsd:reinstall', 'typescript:build', 'jasmine_node:unit'
+		'tsd:reinstall', 'wait:typings', 'typescript:build', 'jasmine_node:unit'
 	]);
-	grunt.registerTask('dev', [
-		'typescript:build', 'jasmine_node:unit', 'watch:ts'
-	]);
+	grunt.registerTask('dev', function () {
+		GruntConfiguration.typescriptReferences(__dirname + '/build/references.ts', [
+			__dirname + '/src', 
+			__dirname + '/tests', 
+			__dirname + '/typings'
+		]);
+		grunt.task.run('typescript:build', 'jasmine_node:unit', 'watch:ts');
+	});
 	grunt.registerTask('postinstall', function () {
 		grunt.task.run('default');
+		if (typeof process.env.NODE_ENV === 'undefined') {
+			grunt.task.run('shell:link_module_util');
+		}
 	});
 	grunt.registerTask('test', [
 		'jasmine_node:unit'
