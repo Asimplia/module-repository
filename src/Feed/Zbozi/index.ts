155a46e37b8f19b5074230828f7a9a392993b9ce
Michael Zabka <zabka.michael@gmail.com>
2015-02-20 13:12:29 +0100

    Export zbozi recorders

---
+++ src/Feed/Zbozi/index.ts
@@ -0,0 +1,5 @@
+
+export import ZboziProductRecorder = require('./ZboziProductRecorder');
+export import ZboziVariantRecorder = require('./ZboziVariantRecorder');
+ZboziProductRecorder;
+ZboziVariantRecorder;
