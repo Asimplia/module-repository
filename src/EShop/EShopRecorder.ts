15ad58d32897c495cd93e670d7426eb0e223c3b4
Michael Zabka <zabka.michael@gmail.com>
2015-02-21 13:56:32 +0100

    Add EShop recorder

---
+++ src/EShop/EShopRecorder.ts
@@ -0,0 +1,23 @@
+
+import EShop = require('../Entity/EShop/EShop');
+import SqlExecutor = require('../Util/SqlExecutor');
+
+export = EShopRecorder;
+class EShopRecorder {
+	
+	private sqlExecutor: SqlExecutor;
+
+	static $service = 'EShop.EShopRecorder';
+	static $inject = [
+		'connection.postgres'
+	];
+	constructor(
+		private connection: any
+	) {
+		this.sqlExecutor = new SqlExecutor(connection, EShop, EShop.COLUMN_E_SHOP_ID, 'id');
+	}
+
+	insert(eShop: EShop, callback: (e: Error, eShop?: EShop) => void) {
+		this.sqlExecutor.insert(eShop, callback);
+	}
+}
