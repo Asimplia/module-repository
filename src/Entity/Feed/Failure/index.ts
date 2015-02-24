b7aab8c26442499cb3968b9548ae66d3903716a2
Michael Zabka <zabka.michael@gmail.com>
2015-02-24 22:25:05 +0100

    Export ValueFailureConverter

--- src/Entity/Feed/Failure/index.ts
+++ src/Entity/Feed/Failure/index.ts
@@ -1,4 +1,6 @@
 
 export import ValueFailure = require('./ValueFailure');
+export import ValueFailureConverter = require('./ValueFailureConverter');
 export import IValueFailureObject = require('./IValueFailureObject');
 ValueFailure;
+ValueFailureConverter;
