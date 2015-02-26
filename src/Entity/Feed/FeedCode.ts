7fe51044fc33fcb1bd9fc92389b3dd1a78a08822
Michael Zabka <zabka.michael@gmail.com>
2015-02-26 18:14:45 +0100

    Add FeedCode as enum

---
+++ src/Entity/Feed/FeedCode.ts
@@ -0,0 +1,9 @@
+
+export = FeedCode;
+enum FeedCode {
+	HEUREKA,
+	ZBOZI,
+	SITEMAP,
+	PRICE_API,
+	GOOGLE_ANALYTICS
+}
--- src/Entity/Feed/index.ts
+++ src/Entity/Feed/index.ts
@@ -1,10 +1,12 @@
 
 export import FeedLoad = require('./FeedLoad');
+export import FeedCode = require('./FeedCode');
 export import Zbozi = require('./Zbozi/index');
 export import Heureka = require('./Heureka/index');
 export import Failure = require('./Failure/index');
 export import Check = require('./Check/index');
 FeedLoad;
+FeedCode;
 Zbozi;
 Heureka;
 Failure;
