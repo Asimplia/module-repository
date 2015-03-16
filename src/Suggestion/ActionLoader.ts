687fb2b3381bfde112be37e2a4577c2cd00993d1
Michael Zabka <zabka.michael@gmail.com>
2015-03-16 16:59:35 +0100

    Add action getter filtered by section

--- src/Suggestion/ActionLoader.ts
+++ src/Suggestion/ActionLoader.ts
@@ -5,6 +5,8 @@ import Factor = require('../Entity/Factor/Factor');
 import mongoose = require('mongoose');
 import ActionModel = require('../Definition/Suggestion/ActionModel');
 import DocumentExecutor = require('../Util/DocumentExecutor');
+import SectionEnum = require('../Entity/Section/SectionEnum');
+import _ = require('underscore');
 
 export = ActionLoader;
 class ActionLoader {
@@ -22,6 +24,24 @@ class ActionLoader {
 
 	getList(callback: (e: Error, actionList?: List<SuggestionAction>) => void) {
 		this.model.find({}, null, { sort: 'id' }, (e, actions: mongoose.Document[]) => {
+			if (e) {
+				return callback(e);
+			}
+			var list = new List<SuggestionAction>();
+			list.pushArray(actions, SuggestionAction.fromObject);
+			callback(e, list);
+		});
+	}
+
+	getListBySections(sections: SectionEnum[], callback: (e: Error, actionList?: List<SuggestionAction>) => void) {
+		var conditions = {
+			section: {
+				$in: _.map(sections, (section: SectionEnum) => {
+					return SectionEnum[section];
+				})
+			}
+		};
+		this.model.find(conditions, null, { sort: 'id' }, (e, actions: mongoose.Document[]) => {
 			if (e) {
 				return callback(e);
 			}
