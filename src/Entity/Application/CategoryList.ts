e030d43f0248bd89622a9b350c0bd40e3a59fd8f
Michael Zabka <zabka.michael@gmail.com>
2015-04-13 00:27:09 +0200

    Add category list

---
+++ src/Entity/Application/CategoryList.ts
@@ -0,0 +1,12 @@
+
+import Util = require('asimplia-util');
+import Category = require('./Category');
+import List = Util.ODBM.Entity.List;
+/* tslint:disable */
+Util;
+/* tslint:enable */
+
+export = CategoryList;
+class CategoryList extends List<Category> {
+
+}
