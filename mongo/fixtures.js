4aa462d400d73172367a0b133cb730ae7764efaa
Michael Zabka <zabka.michael@gmail.com>
2015-06-04 18:58:26 +0200

    Add mongo fixtures

---
+++ mongo/fixtures.js
@@ -0,0 +1,17 @@
+// eshops
+{
+    id: 1,
+    name: "Farfalia",
+    serviceConnections: [],
+    url: "farfalia.com",
+    dateCreated: ISODate("2015-06-03T00:00:00"),
+    imageUri: null
+}
+
+// companies
+{
+    id: 1,
+    name: "Farfalia",
+    vatNumber: "0113",
+    dateCreated: ISODate("2015-06-03T00:00:00")
+}
