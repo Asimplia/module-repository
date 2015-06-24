1833844ec2174329a3f357b8b70e76d6b07aecac
Michael Zabka <zabka.michael@gmail.com>
2015-06-24 20:16:43 +0200

    Change update_product rutine to be created by masterproduct

--- sql/create/03-rutines.sql
+++ sql/create/03-rutines.sql
@@ -118,65 +118,6 @@ $$ LANGUAGE plpgsql;
 
 
 
--- product fullfilling
-CREATE OR REPLACE FUNCTION feed.update_product(loadlogid INT8)
-RETURNS void AS $$
-BEGIN
-	NOTIFY "feed.update_product.start";
-	UPDATE warehouse.product
-	SET productname = source.productname,
-		datechanged = now()
-	FROM (
-	SELECT
-		eshopid, productname, uri
-	FROM feed.get_masterproduct(loadlogid)
-	) as source (
-		eshopid, productname, uri
-	)
-	WHERE product.eshopid = source.eshopid
-		AND product.uri = source.uri;
-	NOTIFY "feed.update_product.tick";
-
-	INSERT INTO warehouse.product
-	(
-		eshopid,
-		productname,
-		baseprice,
-		vat,
-		datecreated,
-		datechanged,
-		flaginshop,
-		inshopfrom,
-		useinmatrices,
-		uri,
-		imageurl
-	)
-	(
-		SELECT
-			source.eshopid,
-			source.productname,
-			0,
-			0,
-			now(),
-			now(),
-			TRUE,
-			now(),
-			TRUE,
-			source.uri,
-			source.imageurl
-		FROM feed.get_masterproduct(loadlogid) source
-		LEFT JOIN warehouse.product
-			ON product.eshopid = source.eshopid
-			AND product.uri = source.uri
-		WHERE product.productid IS NULL
-	);
-	NOTIFY "feed.update_product.done";
-END;
-$$ LANGUAGE plpgsql;
-
-
-
-
 -- masterproduct fullfilling
 CREATE OR REPLACE FUNCTION feed.update_masterproduct_sitemap(
 	v_loadid INT8
@@ -376,6 +317,80 @@ BEGIN
 	PERFORM feed.update_masterproduct_priceapi(v_loadlogid);
 
 	NOTIFY "feed.update_masterproduct.done";
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+
+-- product fullfilling
+CREATE OR REPLACE FUNCTION feed.update_product(v_eshopid INT8)
+RETURNS void AS $$
+BEGIN
+	NOTIFY "feed.update_product.start";
+	INSERT INTO warehouse.product
+	(
+		eshopid,
+		productname,
+		baseprice,
+		vat,
+		datecreated,
+		datechanged,
+		flaginshop,
+		inshopfrom,
+		useinmatrices,
+		uri,
+		imageurl
+	)
+	(
+		SELECT
+			source.eshopid,
+			source.productname,
+			0,
+			0,
+			now(),
+			now(),
+			TRUE,
+			now(),
+			TRUE,
+			source.uri,
+			NULL
+		FROM feed.masterproduct source
+		LEFT JOIN warehouse.product
+			ON product.eshopid = source.eshopid
+			AND product.uri = source.uri
+		WHERE product.productid IS NULL
+			AND source.eshopid = v_eshopid
+			AND (
+				source.heurekaid IS NOT NULL
+				OR source.zboziid IS NOT NULL
+				OR source.revenuesid IS NOT NULL
+			)
+	);
+
+	NOTIFY "feed.update_product.tick";
+	UPDATE warehouse.product
+	SET productname = source.productname,
+		datechanged = now(),
+		imageurl = COALESCE(zbozi.imgurl, heureka.imgurl, heureka.imgurl_alternative, priceapi.image_url)
+	FROM (
+		SELECT
+			eshopid, productname, uri, zboziid, heurekaid, priceapiid
+		FROM feed.masterproduct
+		WHERE masterproduct.eshopid = v_eshopid
+	) as source (
+		eshopid, productname, uri
+	)
+	LEFT JOIN feed.zbozi
+		ON zbozi.zboziid = source.zboziid
+	LEFT JOIN feed.heureka
+		ON heureka.heurekaid = source.heurekaid
+	LEFT JOIN feed.priceapi
+		ON priceapi.priceapiid = source.priceapiid
+	WHERE product.eshopid = source.eshopid
+		AND product.uri = source.uri;
+
+	NOTIFY "feed.update_product.done";
 END;
 $$ LANGUAGE plpgsql;
 
---
+++ sql/patches/2015-06-24_update_product.sql
@@ -0,0 +1,72 @@
+DROP FUNCTION feed.update_product(loadlogid INT8);
+
+-- product fullfilling
+CREATE OR REPLACE FUNCTION feed.update_product(v_eshopid INT8)
+RETURNS void AS $$
+BEGIN
+	NOTIFY "feed.update_product.start";
+	INSERT INTO warehouse.product
+	(
+		eshopid,
+		productname,
+		baseprice,
+		vat,
+		datecreated,
+		datechanged,
+		flaginshop,
+		inshopfrom,
+		useinmatrices,
+		uri,
+		imageurl
+	)
+	(
+		SELECT
+			source.eshopid,
+			source.productname,
+			0,
+			0,
+			now(),
+			now(),
+			TRUE,
+			now(),
+			TRUE,
+			source.uri,
+			NULL
+		FROM feed.masterproduct source
+		LEFT JOIN warehouse.product
+			ON product.eshopid = source.eshopid
+			AND product.uri = source.uri
+		WHERE product.productid IS NULL
+			AND source.eshopid = v_eshopid
+			AND (
+				source.heurekaid IS NOT NULL
+				OR source.zboziid IS NOT NULL
+				OR source.revenuesid IS NOT NULL
+			)
+	);
+
+	NOTIFY "feed.update_product.tick";
+	UPDATE warehouse.product
+	SET productname = source.productname,
+		datechanged = now(),
+		imageurl = COALESCE(zbozi.imgurl, heureka.imgurl, heureka.imgurl_alternative, priceapi.image_url)
+	FROM (
+		SELECT
+			eshopid, productname, uri, zboziid, heurekaid, priceapiid
+		FROM feed.masterproduct
+		WHERE masterproduct.eshopid = v_eshopid
+	) as source (
+		eshopid, productname, uri
+	)
+	LEFT JOIN feed.zbozi
+		ON zbozi.zboziid = source.zboziid
+	LEFT JOIN feed.heureka
+		ON heureka.heurekaid = source.heurekaid
+	LEFT JOIN feed.priceapi
+		ON priceapi.priceapiid = source.priceapiid
+	WHERE product.eshopid = source.eshopid
+		AND product.uri = source.uri;
+
+	NOTIFY "feed.update_product.done";
+END;
+$$ LANGUAGE plpgsql;
